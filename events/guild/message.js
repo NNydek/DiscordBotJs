6c1c4e9705735394ef9ec258aaa7d0430fdcc491
NNydek <nydeous17@gmail.com>
2021-10-03 20:52:52 +0200

    add 'try catch' to message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -7,5 +7,11 @@ module.exports = (Discord, client, message) => {
 
     const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd));
 
-    if(command) command.execute(client, message, args, Discord);
+    try{
+        command.execute(client, message, args, Discord);
+    } catch (err) {
+        message.reply("Something went wrong! Try again.");
+        return err;
+    }
+        
 }
